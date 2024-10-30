c2902a538f00d9865933acf381568ca42c5c1be0
Jason Hawgood <jason@seavistasoftware.com>
2024-10-30 14:20:51 -0400

    Prepare for deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //reactStrictMode: true,
   output: 'export', // Enables static export
   basePath: '/at5ai-web', // Leading slash is required
   trailingSlash: true, // Ensures paths work correctly on GitHub Pages
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-
+'use client';
 
 import styles from '../styles/home.module.css'
 

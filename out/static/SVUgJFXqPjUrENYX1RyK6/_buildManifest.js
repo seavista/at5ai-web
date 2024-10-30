3b7ded2848c0fcb887f415cc783298f7b835d3ed
Jason Hawgood <jason@seavistasoftware.com>
2024-10-30 12:32:07 -0400

    Deploy to GitHub Pages

---
+++ out/static/SVUgJFXqPjUrENYX1RyK6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/0bd39407ab76ce2c.css","static/chunks/pages/index-b53ae917ae873cd2.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

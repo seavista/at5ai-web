07c9b5a58509daa90cbe4a7cbba2f2c15ea84e5e
Jason Hawgood <jason@seavistasoftware.com>
2024-10-30 15:12:34 -0400

    jth

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   //reactStrictMode: true,
   output: 'export', // Enables static export
-  basePath: '/at5ai-web', // Leading slash is required
-  trailingSlash: true, // Ensures paths work correctly on GitHub Pages
-  images: {
-    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
-  },
+  reactStrictMode: true,
+ // basePath: '/at5ai-web', // Leading slash is required
+ // trailingSlash: true, // Ensures paths work correctly on GitHub Pages
+  // images: {
+  //   unoptimized: true, // Disable Next.js image optimization for GitHub Pages
+  // },
 };
 
 module.exports = nextConfig;
